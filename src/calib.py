d38828398e73c02a2578e679ffbec5583a32cc65
b3n1jr <47711426+b3nj1r@users.noreply.github.com>
2021-06-05 03:13:28 -0700

    seperated logic into functions

--- src/calib.py
+++ src/calib.py
@@ -1,55 +1,71 @@
 import numpy as np
+import pickle
 import glob
 import cv2
 import sys
 
 
-def calib(images):
-    pass
+def calc(arr):
 
+    """ camera matrix calculation
 
-def main():
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    arr, str
+
+    """
 
     # initialize object points
     objp = np.zeros((6*9,3),np.float32)
     objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
-    print(objp)
 
     # obj and img point arrays
     objpts = [] # 3d real world space
     imgpts = [] # 2d image space
 
-    images = glob.glob(str(sys.argv[1]))
 
-    for f in images:
-        # read image and convert to grayscale
-        img = cv2.imread(f)
-        gry = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # read image and convert to grayscale
+    img = cv2.imread(arr)
+    gry = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-        # solve for corners
-        ret, corners = cv2.findChessboardCorners(gry,(9,6),None)
+    # solve for corners
+    ret, corners = cv2.findChessboardCorners(gry,(9,6),None)
 
-        if ret:
-            # update point arrays
-            objpts.append(objp)
-            imgpts.append(corners)
+    if ret:
+        # update point arrays
+        objpts.append(objp)
+        imgpts.append(corners)
 
-        # camera matrix, distortion coefficients, radial and translational vectors
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpts, imgpts, gry.shape[::-1], None, None)
+    # img width and height
+    h,w = img.shape[:2]
 
-        # undistortion
-        h,w = img.shape[:2]
-        newmtx,roi = cv2.getOptimalNewCameraMatrix(mtx,dist, (w,h), 1, (w,h))
+    # camera matrix, distortion coefficients, radial and translational vectors
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpts, imgpts, gry.shape[::-1], None, None)
+    newmtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist, (w,h), 1, (w,h))
+    return ret, mtx, newmtx, dist, rvecs, tvecs
 
-        xmap, ymap = cv2.initUndistortRectifyMap(mtx,dist,None,newmtx,(w,h),5)
-        dst = cv2.remap(img, xmap, ymap, cv2.INTER_LINEAR)
 
-        x,y,w,h = roi
-        dst = dst[y:y+h, x:x+w]
-        cv2.imwrite('../calib/result.jpg',dst)
+def rectify(arr, mtx, newmtx, dist):
+
+    """ image rectification
+
+    arr, str
+    mtx, ndarray
+    newmtx, ndarray
+    dist, ndarray
+
+    """
+
+    img = cv2.imread(arr)
+    h,w = img.shape[:2]
+
+    xmap, ymap = cv2.initUndistortRectifyMap(mtx,dist,None,newmtx,(w,h),5)
+    return cv2.remap(img, xmap, ymap, cv2.INTER_LINEAR)
+
+
+
+def main():
+    ret, mtx, newmtx, dist, rvecs, tvecs = calc(str(sys.argv[1]))
+
+    cv2.imwrite('../calib/result.jpg',rectify(str(sys.argv[2]), mtx, newmtx, dist))
 
 if __name__ =="__main__":
-    main()
-    for i, arg in enumerate(sys.argv):
-        print(arg)
+   main()
